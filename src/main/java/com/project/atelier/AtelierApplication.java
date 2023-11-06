32270d516e6977ff61e0d4082c4efbab813ed269
marcoantonio40 <marcoudi40@gmail.com>
2023-11-06 08:18:30 -0300

    Fixed

--- src/main/java/com/project/atelier/AtelierApplication.java
+++ src/main/java/com/project/atelier/AtelierApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AtelierApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AtelierApplication.class, args);
 	}
