3fb8f7972d8ae4ee482569565238cf73b63cdd67
marcoantonio40 <marcoudi40@gmail.com>
2023-11-07 08:26:39 -0300

    Added swagger

--- src/main/java/com/project/atelier/AtelierApplication.java
+++ src/main/java/com/project/atelier/AtelierApplication.java
@@ -1,5 +1,6 @@
 package com.project.atelier;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/com/project/atelier/controller/LoginController.java
+++ src/main/java/com/project/atelier/controller/LoginController.java
@@ -1,17 +1,24 @@
 package com.project.atelier.controller;
 
 import com.project.atelier.dto.request.LoginRequest;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-@RestController
-@RequestMapping(value = "/test")
-public interface LoginController {
 
-    @GetMapping
-    String login(@Valid LoginRequest request);
+@RequestMapping(value = "/login", produces = {"application/json"})
+@RestController
+@Tag(name = "open-api")
+public class LoginController {
+
+    @Operation(summary = "Realiza login na aplicação", method = "POST")
+    @PostMapping(path = "/token")
+    @ResponseBody
+    ResponseEntity<String> login(@Valid @RequestBody LoginRequest request){
+        return ResponseEntity.accepted().build();
+    }
 
 }
