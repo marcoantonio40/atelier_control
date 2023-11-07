a223a7967cdd61c2fa2013ae201e929488a4caac
marcoantonio40 <marcoudi40@gmail.com>
2023-11-07 09:43:27 -0300

    Removed swagger

--- src/main/java/com/project/atelier/controller/LoginController.java
+++ src/main/java/com/project/atelier/controller/LoginController.java
@@ -1,8 +1,6 @@
 package com.project.atelier.controller;
 
 import com.project.atelier.dto.request.LoginRequest;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,12 +9,9 @@ import javax.validation.Valid;
 
 @RequestMapping(value = "/login", produces = {"application/json"})
 @RestController
-@Tag(name = "open-api")
 public class LoginController {
 
-    @Operation(summary = "Realiza login na aplicação", method = "POST")
     @PostMapping(path = "/token")
-    @ResponseBody
     ResponseEntity<String> login(@Valid @RequestBody LoginRequest request){
         return ResponseEntity.accepted().build();
     }
