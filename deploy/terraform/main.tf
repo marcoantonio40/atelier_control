7699f23bbe79934e77ddcbe3ee0e6c759ff82ec7
marcoantonio40 <marcoudi40@gmail.com>
2023-12-10 10:14:57 -0300

    More aws resources

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -17,3 +17,38 @@ resource "aws_subnet" "atelier_control_subnet" {
     Name = "atelier_control_subnet_pub"
   }
 }
+
+resource "aws_internet_gateway" "atelier_control_internet_gateway" {
+  vpc_id = aws_vpc.atelier_control_vpc.id
+
+  tags = {
+    Name = "atelier_control_internet_gateway"
+  }
+}
+
+resource "aws_route_table" "atelier_control_route_table" {
+  vpc_id = aws_vpc.atelier_control_vpc.id
+
+  tags = {
+    Name = "atelier_control_route_table"
+  }
+}
+
+resource "aws_route" "atelier_control_route" {
+  route_table_id = aws_route_table.atelier_control_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.atelier_control_internet_gateway.id
+
+  tags = {
+    Name = "atelier_control_route"
+  }
+}
+
+resource "aws_route_table_association" "atelier_control_route_table_association" {
+  route_table_id = aws_route_table.atelier_control_route_table.id
+  subnet_id = aws_subnet.atelier_control_subnet.id
+
+  tags = {
+    Name = "atelier_control_route_table_association"
+  }
+}
