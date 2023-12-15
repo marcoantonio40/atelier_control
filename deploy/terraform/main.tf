36288f2453e9936cc96f19b87b6d2a5f7163c742
marcoantonio40 <marcoudi40@gmail.com>
2023-12-15 07:42:00 -0300

    Formantter code terraform

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -9,9 +9,9 @@ resource "aws_vpc" "atelier_control_vpc" {
 }
 
 resource "aws_subnet" "atelier_control_subnet" {
-  vpc_id            = aws_vpc.atelier_control_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-2a"
+  vpc_id                  = aws_vpc.atelier_control_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-2a"
   map_public_ip_on_launch = true
 
   tags = {
