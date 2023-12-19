163b86e9565cc5abb7a08001679a832d393e26d8
marcoantonio40 <marcoudi40@gmail.com>
2023-12-18 21:11:43 -0300

    Removed unnecessary imports

--- src/main/java/com/project/atelier/model/Order.java
+++ src/main/java/com/project/atelier/model/Order.java
@@ -2,7 +2,6 @@ package com.project.atelier.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
