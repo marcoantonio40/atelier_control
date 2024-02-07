086f1b04a1e3aaf7a75173ba23919c5c0a346265
marcoudi40 <marcoudi40@gmail.com>
2024-02-07 15:19:17 -0300

    Conexão com o banco de dados local

--- src/main/java/com/project/atelier/model/User.java
+++ src/main/java/com/project/atelier/model/User.java
@@ -6,7 +6,6 @@ import com.project.atelier.model.generics.DefaultEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
